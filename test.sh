0bd048e10cf6c6cf51865e7ee3b1c8e070057763
Rian Douglas <rian.douglas@thetradedesk.com>
2020-06-05 12:18:38 -0700

    Dunify the build process

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ echo 'running tests...'
 ulimit -s unlimited
 wordsfile=/usr/share/dict/words
 baseaddr=0x440000000000 # System specific - see README.txt
-./test_ancient_dict_write.opt $wordsfile dictionary.data $baseaddr
-./test_ancient_dict_verify.opt $wordsfile dictionary.data
-./test_ancient_dict_read.opt dictionary.data <<EOF
+./_build/default/test_ancient_dict_write.exe $wordsfile dictionary.data $baseaddr
+./_build/default/test_ancient_dict_verify.exe $wordsfile dictionary.data
+./_build/default/test_ancient_dict_read.exe dictionary.data <<EOF
 dog
 cat
 q
