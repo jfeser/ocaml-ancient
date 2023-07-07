0064ee49fb2d024598f72f5c015e52c7b24768a1
Jack Feser <feser@mit.edu>
2023-07-07 12:30:41 -0400

    add nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -6,20 +6,27 @@
   outputs = { self, flake-utils, nixpkgs }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        ocamlPkgs = pkgs.ocaml-ng.ocamlPackages;
-        ancient = ocamlPkgs.buildDunePackage {
-          pname = "ancient";
-          version = "0.1";
-          useDune3 = true;
-          src = ./.;
+        overlay = self: super: {
+          ocamlPackages = super.ocamlPackages.overrideScope' (oself: osuper: {
+            ancient = oself.buildDunePackage {
+              pname = "ancient";
+              version = "0.1";
+              useDune3 = true;
+              src = ./.;
+            };
+          });
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
         };
       in {
-        defaultPackage = ancient;
+        overlay = overlay;
+        defaultPackage = pkgs.ocamlPackages.ancient;
         devShell = pkgs.mkShell {
           nativeBuildInputs =
             [ pkgs.ocamlformat pkgs.opam pkgs.ocamlPackages.ocaml-lsp ];
-          inputsFrom = [ ancient ];
+          inputsFrom = [ self.defaultPackage.${system} ];
         };
       });
 }
